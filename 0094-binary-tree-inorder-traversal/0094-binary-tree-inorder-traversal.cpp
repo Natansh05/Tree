7370e6e1e67e0bed0b880375ba2d7eb88a94285f
noob_coder005 <202201445@daiict.ac.in>
2023-12-27 23:34:31 +0530

    Both approaches added with Time & Space Complexity

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -9,10 +9,35 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//T.C. - O(N)
+//S.C. - O(N)
+
+//striver's vid link - https://www.youtube.com/watch?v=lxTGsVXjwvM&list=PLgUwDviBIf0q8Hkd7bK2Bpryj2xVJk8Vk&index=11
+
+
+class Solution {
+public:
+    //recursive approach
+    vector<int> ans;
+    void inorder(TreeNode* root){
+        if(root==NULL) 
+            return;
+        inorder(root->left);
+        ans.push_back(root->val);
+        inorder(root->right);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        ios_base::sync_with_stdio(0);
+        inorder(root);
+        return ans;
+    }
+};
+
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         ios_base::sync_with_stdio(0);
+        //iterative version;
         stack<TreeNode*> st;
         TreeNode* node = root;
         vector<int> ans;

e33d6ce262ef84f03854a444c0fbe716f50a34cc
natansh05 <202201445@daiict.ac.in>
2024-03-02 00:35:27 +0530

    Update isBalanced.cpp

--- isBalanced.cpp
+++ isBalanced.cpp
@@ -1,3 +1,5 @@
+
+//used pair to store previous value
 class Solution {
 public:
 
@@ -29,5 +31,31 @@ public:
 
     bool isBalanced(TreeNode* root) {
         return IsBalancedFast(root).first;
+    }
+};
+
+
+
+//recursive approach
+class Solution {
+    private:
+    int height(TreeNode *root){
+        if(root==NULL)
+            return 0;
+        int left = height(root->left);
+        if(left==-1)
+            return -1;
+        int right = height(root->right);
+        if(right == -1)
+            return -1;
+        int diff = abs(left-right);
+        if(diff>1)
+            return -1;
+        return max(left,right)+1;
+    }
+public:
+    bool isBalanced(TreeNode* root) {
+        ios_base::sync_with_stdio(0);
+        return height(root)!=-1;
     }
 };

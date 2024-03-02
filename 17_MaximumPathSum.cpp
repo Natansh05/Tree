3ed9b0779c6ca37eda58c188d209b142bfc49795
natansh05 <202201445@daiict.ac.in>
2024-03-02 14:27:04 +0530

    Create 17_MaximumPathSum.cpp

---
+++ 17_MaximumPathSum.cpp
@@ -0,0 +1,21 @@
+// problem link - https://leetcode.com/problems/binary-tree-maximum-path-sum/
+//Difficulty - Hard
+
+class Solution {
+    private:
+    int solve(TreeNode* root,int &maxi){
+        if(root==NULL)
+            return 0;
+        int left = max(0,solve(root->left,maxi));
+        int right = max(0,solve(root->right,maxi));
+        maxi = max(maxi,left+right+root->val);
+        return max(left,right)+root->val;
+    }
+public:
+    int maxPathSum(TreeNode* root) {
+        ios_base::sync_with_stdio(0);
+        int max = INT_MIN;
+        solve(root,max);
+        return max;
+    }
+};

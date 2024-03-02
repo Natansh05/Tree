1a0ae6c51471fe33a22ff6b5bdc1fcfd1d78b4d9
natansh05 <202201445@daiict.ac.in>
2024-03-02 22:29:39 +0530

    Create 25_isSymmetric.cpp

---
+++ 25_isSymmetric.cpp
@@ -0,0 +1,15 @@
+// link - https://leetcode.com/problems/symmetric-tree/description/
+
+class Solution {
+public:
+
+    bool isMirror(TreeNode* n1,TreeNode* n2){
+        if(n1==NULL && n2 ==NULL) return true;
+        if(n1==NULL || n2 ==NULL) return false;
+        return n1->val == n2->val && isMirror(n1->right,n2->left) && isMirror   (n1->left,n2->right);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+            return isMirror(root,root);
+    }
+};

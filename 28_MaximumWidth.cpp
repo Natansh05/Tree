1a4d7bbee00a229011e40abfba386c95f0e0f5d0
natansh05 <202201445@daiict.ac.in>
2024-03-03 15:48:08 +0530

    Create 28_MaximumWidth.cpp

---
+++ 28_MaximumWidth.cpp
@@ -0,0 +1,39 @@
+// link - https://leetcode.com/problems/maximum-width-of-binary-tree/
+
+// Time Complexity - O(N)
+// Space Compexity - O(N)
+
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        ios_base::sync_with_stdio(0);
+        if(root==NULL)
+            return 0;
+        int ans = 0;
+
+        queue<pair<TreeNode*,int>> q;
+        q.push({root,0});
+
+        while(!q.empty()){
+            long long size = q.size();
+            long long int mmin = q.front().second;
+            int first , last;
+            for(int i=0;i<size;++i){
+                long long cur_id = q.front().second-mmin;
+                TreeNode* node = q.front().first;
+                q.pop();
+
+                if(i==0)
+                    first = cur_id;
+                if(i==size-1)
+                    last = cur_id;
+                if(node->left)
+                    q.push({node->left,cur_id*2+1});
+                if(node->right)
+                    q.push({node->right,cur_id*2+2});
+            }
+            ans = max(ans,last-first+1);
+        }
+        return ans;
+    }
+};

e351fb9bf9a09b338f403690a448ee4b180d6207
natansh05 <202201445@daiict.ac.in>
2024-06-02 16:35:11 +0530

    Update 18_isSame.cpp

--- 18_isSame.cpp
+++ 18_isSame.cpp
@@ -1,22 +1,6 @@
 //link - https://leetcode.com/problems/same-tree/
 
-class Solution {
-public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        ios_base::sync_with_stdio(0);
-        if(p==NULL && q==NULL) return true;
-        else if(p!=NULL && q==NULL) return false;
-        else if(p==NULL && q!=NULL) return false;
-
-        bool left = isSameTree(p->left,q->left);
-        bool right = isSameTree(p->right,q->right);
-        bool value = p->val == q->val;
-        
-        if(left && right && value) return 1;
-        else return 0;
-    }
-};
-
+// Time , Space Complexity - O(N)
 
 class Solution {
 public:
